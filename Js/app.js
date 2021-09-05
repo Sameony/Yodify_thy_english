0ed3d203d7f362a4b5e84311a5fd47fb20a0f1c3
Sameony <iam2001sam@gmail.com>
2021-09-05 22:05:12 +0530

    took input from textarea

--- Js/app.js
+++ Js/app.js
@@ -1,6 +1,10 @@
 var btn = document.querySelector("#btn-check");
-
-btn = addEventListener("click",function clickEventHandler()
+var txt_input = document.querySelector("#txt-input");
+console.log(txt_input);
+function clickEventHandler()
 {
     console.log("You clicked it!!!!");
-})
+    console.log("input is",txt_input.value);
+}
+btn = addEventListener("click",clickEventHandler);
+
