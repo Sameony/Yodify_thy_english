319d7c13a79ceac4ba74fe025d4cf927c685581e
Sameony <iam2001sam@gmail.com>
2021-09-06 00:25:20 +0530

    input output processing

--- Js/app.js
+++ Js/app.js
@@ -1,10 +1,35 @@
 var btn = document.querySelector("#btn-check");
 var txt_input = document.querySelector("#txt-input");
-console.log(txt_input);
+var txt_output = document.querySelector("#txt-output");
+
+var serverURL ="https://api.funtranslations.com/translate/yoda.json";
+
+
+/* fetching n stuffs*/ 
+function makeUrl(text1)
+{
+    return (serverURL+"?" + "text=" + text1);
+}
+function errorHandler(error)
+{
+    console.log("Unexpected error occurred: "+error);
+}
+function fetchit(url)
+{
+    fetch(url)
+    .then(response=>response.json())
+    .then(json=>{
+        txt_output.innerText=json.contents.translated})
+    .catch(errorHandler)
+}
+
+
 function clickEventHandler()
 {
     console.log("You clicked it!!!!");
-    console.log("input is",txt_input.value);
+    fetchit(makeUrl(txt_input.value));
+    
 }
+
 btn = addEventListener("click",clickEventHandler);
 
